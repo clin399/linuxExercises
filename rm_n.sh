85324884d080889b82eff9af6539fcc440f8890a
Patrick <plklein@wisc.edu>
2026-03-03 09:16:59 -0600

    Fixed changes in rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -7,12 +7,13 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-dir = "$1"
-n = "$2"
+dir="$1"
+n="$2"
 #Make sure directory exists
 
-if [ ! -d "$dir"]; then
+if [ ! -d "$dir" ]; then
     echo "error: '$dir' is not a directory" 1>&2
+    exit 1
 fi
 #Reomve files larger than n bytes
 find "$dir" -type f -size +"$n"c -exec rm {} \;
